6dd01910f4adacca48aa811fd3d58945726e52b6
Jesus Sebastian Berumen <23040983@itdurango.edu.mx>
2026-06-06 19:29:58 -0600

    Add files via upload

--- cliente-angular/src/app/app.routes.ts
+++ cliente-angular/src/app/app.routes.ts
@@ -12,14 +12,32 @@ from './features/usuarios/pages/usuarios/usuarios';
 
 import { authGuard }
 from './core/guards/auth-guard';
+import { Menu } from './features/cliente/menu/pages/menu/menu';
+import { Carrito } from './features/cliente/carrito/pages/carrito/carrito';
+import { Pedido } from './features/cliente/pedido/pages/pedido/pedido';
 
 export const routes: Routes = [
-
   {
     path: '',
+    redirectTo: 'm/1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'm/:mesa',
+    component: Menu
+  },
+  {
+  path: 'm/:mesa/carrito',
+    component: Carrito
+  },
+  {
+    path: 'm/:mesa/pedido',
+    component: Pedido
+  },
 
+  {
+    path: 'admin/login',
     component: LoginComponent
-    
   },
 
   {
